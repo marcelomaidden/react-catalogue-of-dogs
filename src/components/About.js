21b2503cb1010469b8408021b52cb295e9d0d3be
Marcelo Fernandes de Araujo <marcelomaidden@gmail.com>
2021-03-08 14:39:16 -0300

    React single page application
    - Adds react-router-dom package
    - Creates Routes on index.js
    - Adds links on the navigation bar
    - Removes .gitkeep and adds components and containers

---
+++ src/components/About.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>Here is the about page</div>
+  )
+}
+
+export default About;
--- src/containers/NavBar.js
+++ src/containers/NavBar.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const NavBar = () => {
   return (
-    <nav>Here is the navigation bar</nav>
+    <nav>
+      Here is the navigation bar
+      <ul>
+        <li><Link to='/'>Home</Link></li>
+        <li><Link to='/about'>About</Link></li>
+      </ul>
+    </nav>
   )
 }
 
