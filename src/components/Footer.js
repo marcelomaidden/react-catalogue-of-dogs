e8d8d5e8f516b6f81540d79662f4695d9343e3f3
Marcelo Fernandes de Araujo <marcelomaidden@gmail.com>
2021-03-10 17:26:14 -0300

    Adds copyright on footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 import '../stylesheets/Footer.css';
 
 const Footer = () => (
-  <footer className="p-5">Here is the footer</footer>
+  <footer className="p-5">&copy; 2021 - Marcelo Fernandes de Araujo</footer>
 );
 
 export default Footer;
