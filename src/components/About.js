cbe3ba827f247f1f77f58cf232e7a386d76a69df
Marcelo Fernandes de Araujo <marcelomaidden@gmail.com>
2021-03-10 16:30:22 -0300

    Adds some quotes on about page

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,25 @@
 import React from 'react';
 
 const About = () => (
-  <div>Here is the about page</div>
+  <div className="mx-auto">
+    <div className="h6 font-lilita-one m-5">
+      <p>The intention of this website is to show images of dogs. </p>
+      <p>The author of this website is passionate about them so, below</p>
+      <p>there are some quotes that show why they are so important.</p>
+    </div>
+    <ul>
+      <li>
+        “A dog is the only thing on earth that loves you more than you love yourself.”
+        – Josh Billings
+      </li>
+      <li>
+        “Dog is God spelled backward.” – Duane Chapman
+      </li>
+      <li>
+        “No matter how you’re feeling, a little dog gonna love you.” – Waka Flocka Flame
+      </li>
+    </ul>
+  </div>
 );
 
 export default About;
